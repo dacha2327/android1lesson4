17fc2931deedb1afeaa8c04816a7e2484e69e0ba
dacha2327 <dachabas2327@gmail.com>
2022-09-24 15:41:14 +0600

    Initial commit

---
+++ app/src/main/java/com/dacha/android1lesson4/MemFragment.java
@@ -0,0 +1,31 @@
+package com.dacha.android1lesson4;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+
+public class MemFragment extends Fragment {
+    private ImageView imageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mem, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = view.findViewById(R.id.iv_mem);
+        Glide.with(imageView).load("https://www.meme-arsenal.com/memes/570e7be3d6daa2a35c473e89d97640cf.jpg").into(imageView);
+    }
+}
---
+++ app/src/main/java/com/dacha/android1lesson4/OnItemClick.java
@@ -0,0 +1,5 @@
+package com.dacha.android1lesson4;
+
+public interface OnItemClick {
+    void onItemClick(Country country);
+}
